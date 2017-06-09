6aa03279f1463dc176405d185184367c25af12c6
Kathleen Yang <kathleen.yang@users.noreply.github.com>
2017-06-09 02:59:57 -0500

    update 5

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,7 +1,7 @@
 class Startup < ApplicationRecord
-  has_many :cofoundings, :dependent => :destroy
+
   has_many :comments, :dependent => :destroy
-  has_many :founders, :through => :cofoundings, :source => :founder
+  
   belongs_to :user, :class_name => "User", :foreign_key => "User_id"
 validates:User_id,presence: true
 validates:name,uniqueness: true
--- app/models/venturefund.rb
+++ app/models/venturefund.rb
@@ -1,6 +1,10 @@
 class Venturefund < ApplicationRecord
-  has_many :partners, :dependent => :destroy
+  
   has_many :comments, :dependent => :destroy
   belongs_to :user, :class_name => "User", :foreign_key => "User_id"
   validates:User_id,presence: true
+  validates:name,uniqueness: true
+  validates:address,presence: true
+  validates:funding,presence: true
+  validates:intro,presence: true
 end
